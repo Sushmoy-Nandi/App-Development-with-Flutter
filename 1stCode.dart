416d30fef73c5112c517466c13e50f60f0bc1398
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-11-27 18:21:07 +0600

    add

---
+++ 1stCode.dart
@@ -0,0 +1,8 @@
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+  int sum = num1 + num2;
+  int difference = num1 - num2;
+  print("The sum of $num1 and $num2 is: $sum");
+  print("The difference between $num1 and $num2 is: $difference");
+}
