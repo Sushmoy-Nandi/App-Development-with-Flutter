427ac02254e831b597b024ad8a5d31339dd44631
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-12-30 13:32:30 +0600

    add map data type

---
+++ dart_data_type/map.dart
@@ -0,0 +1,12 @@
+void main() {
+  var person = {
+    'name': 'Sushmoy Nandi',
+    'age': 21,
+    'city': 'Dhaka',
+    'height': '5 Feet 6 Inch'
+  };
+
+  print(person);
+  print(person['name']);
+  print(person['height']);
+}
