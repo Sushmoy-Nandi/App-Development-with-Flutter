8089fcc3be52bdf4d7c000ae6d2d166583f989bc
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-12-30 19:53:55 +0600

    add unary operator

---
+++ dart_operators/unary_operator.dart
@@ -0,0 +1,39 @@
+void main() {
+  var x = 10;
+
+  // ++(Prefix)
+
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+
+  //(Postfix)++
+
+  // print(x++);
+  // print(x++);
+  // print(x++);
+  // print(x++);
+  // print(x++);
+  // print(x++);
+
+  // --(Prefix)
+
+  // print(--x);
+  // print(--x);
+  // print(--x);
+  // print(--x);
+  // print(--x);
+  // print(--x);
+
+  // (Postfix)--
+
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+}
