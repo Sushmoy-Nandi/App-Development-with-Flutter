977442e1eba0ff6dbf295a50359999637a6eb582
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-12-31 19:37:03 +0600

    set added

---
+++ dart_set/set.dart
@@ -0,0 +1,8 @@
+void main() {
+  var myCitySet = <String>{'Dhaka', 'Barisal', 'Jashore'};
+
+  //myCitySet.add("Khulna");
+  myCitySet.addAll({"Khulna", 'Rangpur', 'Savar'});
+
+  print(myCitySet);
+}
