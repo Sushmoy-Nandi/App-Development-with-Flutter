65a9b66f77b791269b830403a7ba748ad82e387c
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-12-31 13:32:45 +0600

    list properties added

---
+++ dart_list_properties/list_properties.dart
@@ -0,0 +1,11 @@
+void main() {
+  var city = ['Dhaka'];
+  //var result = city.length;
+  //var result = city.first;
+  //var result = city.last;
+  //var result = city.reversed;
+  //var result = city.isEmpty;
+  //var result = city.isNotEmpty;
+  var result = city.single;
+  print(result);
+}
