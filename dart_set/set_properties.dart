91876233a8b993f5dd5014c869c296c528385d0c
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-01-01 12:31:47 +0600

    set properties added

---
+++ dart_set/set_properties.dart
@@ -0,0 +1,10 @@
+void main() {
+  var myCitySet = <String>{'Dhaka', 'Jashore', 'Savar', 'CoxBazer'};
+  print(myCitySet.first);
+  print(myCitySet.last);
+  print(myCitySet.length);
+  print(myCitySet.isEmpty);
+  print(myCitySet.isNotEmpty);
+  print(myCitySet.hashCode);
+  // print(myCitySet.single); check whether a set contains only one element
+}
