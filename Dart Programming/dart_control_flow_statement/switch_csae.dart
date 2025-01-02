a33c4305b19a72f28d3c54996cc2e6aa5db0eece
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-01-02 16:24:22 +0600

    switch case added

---
+++ Dart Programming/dart_control_flow_statement/switch_csae.dart
@@ -0,0 +1,21 @@
+void main() {
+  var marks = 40;
+
+  switch (marks) {
+    case 80:
+      print("A+");
+      break;
+
+    case 70:
+      print("A");
+      break;
+
+    case 60:
+      print("A-");
+      break;
+
+    default:
+      print("Result Not found");
+      break;
+  }
+}
