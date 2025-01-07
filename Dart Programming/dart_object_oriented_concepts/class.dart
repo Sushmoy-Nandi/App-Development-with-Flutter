e4b388ce8e3fbd10da767361cb3b56a66b67c86b
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-01-07 09:14:11 +0600

    class added

---
+++ Dart Programming/dart_object_oriented_concepts/class.dart
@@ -0,0 +1,10 @@
+import 'myClass.dart';
+
+void main() {
+  var obj = new myClass();
+  obj.addTwoNumber(20, 30);
+  obj.addThreeNumber(5, 6, 8);
+
+  print(obj.MyName);
+  print(obj.Alphabet[0]);
+}
---
+++ Dart Programming/dart_object_oriented_concepts/myClass.dart
@@ -0,0 +1,12 @@
+class myClass {
+  var MyName = "Sushmoy Nandi";
+  var Alphabet = ['A', 'B', 'C'];
+
+  addTwoNumber(int x, int y) {
+    print(x + y);
+  }
+
+  addThreeNumber(int x, int y, int z) {
+    print(x + y + z);
+  }
+}
